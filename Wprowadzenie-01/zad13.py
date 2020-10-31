ee75e5c19b8f7cc23ae3dba4956630b07ff017d6
Ryszard <Yellow-Magic-Duck-of-War@noreply.github.com>
2020-10-31 21:02:09 +0100

    Dopracowałem zadanie 13

--- Wprowadzenie-01/zad13.py
+++ Wprowadzenie-01/zad13.py
@@ -1,7 +1,4 @@
 książki = []
-
-atrybutyKsiążki = ('Tytuł', 'Autor', 'Wydawca', 'Miejsce wydania', 'Rok wydania')
-
 książka = {}
 
 warunek = True
@@ -15,12 +12,28 @@ while warunek == True:
             'tytuł': input("Tytuł: "),
             'autor': input("Autor: "),
             'wydawca': input("Wydawca: "),
-            'miejsce_wydania': input("Miejsce wydania: "),
-            'rok_wydania': input("Rok wydania: ")
+            'miejsce': input("Miejsce wydania: "),
+            'rok': input("Rok wydania: ")
         }
+
+    książki.append(książka[indeksKsiążki])
 
     indeksKsiążki = indeksKsiążki + 1
 
     decyzja = input("Czy chcesz wprowadzić następną książkę? (T/N): ")
 
-    if decyzja == 'N': break
+    while decyzja != 'T' and decyzja != 'N':
+        decyzja = input("Czy chcesz wprowadzić następną książkę? (T/N): ")
+        if decyzja == 'N':
+            break
+
+    if decyzja == 'N':
+        break
+
+def wypiszSpis (spisKsiążek):
+    tekst = "Książka \"{}\" autorstwa {} wydana przez {} w {} {} roku."
+
+    for wartosc in spisKsiążek:
+        print(tekst.format(wartosc['tytuł'],wartosc['autor'],wartosc['wydawca'],wartosc['miejsce'], wartosc['rok']))
+
+wypiszSpis(książki)
